c9e7354b1e4c869c5d733509f31d181c1d735628
SumathiAchar <sumathiachar55@gmail.com>
2018-01-15 14:13:05 +0530

    Add files via upload

---
+++ Shared.Helpers/StringExtension/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace SSI.ContractManagement.Shared.Helpers.StringExtension
+{
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// To the trim.
+        /// </summary>
+        /// <param name="inputValue">The input value.</param>
+        /// <returns></returns>
+        public static string ToTrim(this string inputValue)
+        {
+            if (!string.IsNullOrWhiteSpace(inputValue))
+            {
+                return inputValue.Trim();
+            }
+            return inputValue;
+        }
+    }
+}
