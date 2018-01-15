47d1a7a57f2f83486e008b1f0705364f14b29b42
SumathiAchar <sumathiachar55@gmail.com>
2018-01-15 14:12:12 +0530

    Add files via upload

---
+++ Shared.Helpers/DataAccess/DataSetExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+
+
+namespace SSI.ContractManagement.Shared.Helpers.DataAccess
+{
+    public static class DataSetExtensions
+    {
+        /// <summary>
+        /// Determines whether [is table data populated] [the specified data set].
+        /// </summary>
+        /// <param name="dataSet">The data set.</param>
+        /// <returns>
+        ///   <c>true</c> if [is table data populated] [the specified data set]; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsTableDataPopulated(this DataSet dataSet)
+        {
+            return dataSet != null && dataSet.Tables.Count > 0 && dataSet.Tables[0].Rows.Count > 0;
+        }
+
+        /// <summary>
+        /// Determines whether [is table data populated] [the specified data set].
+        /// </summary>
+        /// <param name="dataSet">The data set.</param>
+        /// <param name="tableIndex">Index of the table in dataset. First table will always have index 0.</param>
+        /// <returns>
+        ///   <c>true</c> if [is table data populated] [the specified data set]; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsTableDataPopulated(this DataSet dataSet, int tableIndex)
+        {
+            return dataSet != null && dataSet.Tables.Count > 0 && dataSet.Tables.Count >= tableIndex && dataSet.Tables[tableIndex].Rows.Count > 0;
+        }
+    }
+}
