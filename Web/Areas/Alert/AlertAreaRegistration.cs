cca4591339c602b83bbc1d83db63964119bf8ee4
SumathiAchar <sumathiachar55@gmail.com>
2018-01-16 13:06:08 +0530

    Add files via upload

---
+++ Web/Areas/Alert/AlertAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace SSI.ContractManagement.Web.Areas.Alert
+{
+    // ReSharper disable once UnusedMember.Global
+    // This class is used to get Alert Area on application start up.
+    public class AlertAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Alert";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Alert_default",
+                "Alert/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
