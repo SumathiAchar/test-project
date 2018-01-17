397beba62fbf45d18eb40818adbc42f5db648d94
SumathiAchar <sumathiachar55@gmail.com>
2018-01-17 10:42:48 +0530

    Create PaymentTableViewModel.cs

---
+++ Web/Areas/PaymentTable/Models/PaymentTableViewModel.cs
@@ -0,0 +1,10 @@
+using System.Data;
+
+namespace SSI.ContractManagement.Web.Areas.PaymentTable.Models
+{
+    public class PaymentTableViewModel
+    {
+        public DataTable CalimFieldValues { get; set; }
+        public string Message { get; set; }
+    }
+}
