8992d770f6c91fce7b0cfa718744d46108725533
SumathiAchar <sumathiachar55@gmail.com>
2018-01-17 10:40:51 +0530

    Add files via upload

---
+++ Web/Areas/Contract/ContractAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace SSI.ContractManagement.Web.Areas.Contract
+{
+    // ReSharper disable once UnusedMember.Global
+    // This class is used to get Contract Area on application start up.
+    public class ContractAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Contract";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Contract_default",
+                "Contract/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
