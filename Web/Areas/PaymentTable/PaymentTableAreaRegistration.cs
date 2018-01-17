f61db4f785421a27c267ed86a3085bd77fba8fce
SumathiAchar <sumathiachar55@gmail.com>
2018-01-17 10:44:26 +0530

    Add files via upload

---
+++ Web/Areas/PaymentTable/PaymentTableAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace SSI.ContractManagement.Web.Areas.PaymentTable
+{
+    // ReSharper disable once UnusedMember.Global
+    // This class is used to get Payment Table Area on application start up.
+    public class PaymentTableAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "PaymentTable";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "PaymentTable_default",
+                "PaymentTable/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
