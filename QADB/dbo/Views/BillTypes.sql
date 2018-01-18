a74fad81f5fccd8f71a86260432c97c1884325d7
SumathiAchar <sumathiachar55@gmail.com>
2018-01-18 14:31:53 +0530

    Create BillTypes.sql

---
+++ QADB/dbo/Views/BillTypes.sql
@@ -0,0 +1,10 @@
+
+/****************************************************/
+  
+--Created By  : Dev  
+--Date        : 1-Aug-2013  
+--Description : View to retrieve Bill type details
+
+/****************************************************/
+
+CREATE VIEW BillTypes AS SELECT * FROM [$(CONTRACTMANAGEMENT_QA)].dbo._BillTypes 
