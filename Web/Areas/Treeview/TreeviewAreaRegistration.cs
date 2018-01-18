396ec45da89f4e69c119f4210a8c989c0f07371a
SumathiAchar <sumathiachar55@gmail.com>
2018-01-18 13:48:05 +0530

    Add files via upload

---
+++ Web/Areas/Treeview/TreeviewAreaRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+namespace SSI.ContractManagement.Web.Areas.Treeview
+{
+    // ReSharper disable once UnusedMember.Global
+    // This class is used to get Tree view Area on application start up.
+    public class TreeviewAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Treeview";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Treeview_default",
+                "Treeview/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
